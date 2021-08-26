c72e5da99fd1bb4cf28db8147ec27e389315d708
Eric Chang <ericsonc510@gmail.com>
2021-08-26 17:45:43 +0800

    feat(util): [screenshot] return public url of screenshot

--- src/utils/screenshot.ts
+++ src/utils/screenshot.ts
@@ -1,12 +1,12 @@
 import Apify from 'apify';
-import { random } from 'lodash';
+import { random, snakeCase } from 'lodash';
 import { Page } from 'puppeteer';
 
 const {
   utils: { log },
 } = Apify;
 
-export const screenshot = async (page: Page, title: string, selector?: string) => {
+export const screenshot = async (page: Page, title: string, selector?: string): Promise<string> => {
   log.debug(`Saving screenshot of ${title} in ${page.url()}.`);
 
   let screenshotBuffer: string | void | Buffer;
@@ -18,10 +18,15 @@ export const screenshot = async (page: Page, title: string, selector?: string) =
     screenshotBuffer = await page.screenshot();
   }
 
-  const key = page.url().replace(/[:/]/g, '_');
-  await Apify.setValue(key + title.replace(/\s/g, '_') + random(), screenshotBuffer, {
+  const key = `${snakeCase(title)}_${random()}`;
+
+  await Apify.setValue(key, screenshotBuffer, {
     contentType: 'image/png',
   });
 
   log.info(`Screenshot of ${title} in ${page.url()} saved.`);
+
+  const keyValueStore = await Apify.openKeyValueStore();
+
+  return keyValueStore.getPublicUrl(key);
 };
