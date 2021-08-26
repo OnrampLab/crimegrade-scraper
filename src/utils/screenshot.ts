15d2c5191a88eebe62baf6950f409f655c7a4b66
Eric Chang <ericsonc510@gmail.com>
2021-08-26 17:34:21 +0800

    feat(util): [screenshot] support taking screenshot of specific element

--- src/utils/screenshot.ts
+++ src/utils/screenshot.ts
@@ -6,10 +6,18 @@ const {
   utils: { log },
 } = Apify;
 
-export const screenshot = async (page: Page, title: string) => {
+export const screenshot = async (page: Page, title: string, selector?: string) => {
   log.debug(`Saving screenshot of ${title} in ${page.url()}.`);
 
-  const screenshotBuffer = await page.screenshot();
+  let screenshotBuffer: string | void | Buffer;
+
+  if (selector) {
+    const element = await page.$(selector);
+    screenshotBuffer = await element.screenshot();
+  } else {
+    screenshotBuffer = await page.screenshot();
+  }
+
   const key = page.url().replace(/[:/]/g, '_');
   await Apify.setValue(key + title.replace(/\s/g, '_') + random(), screenshotBuffer, {
     contentType: 'image/png',
