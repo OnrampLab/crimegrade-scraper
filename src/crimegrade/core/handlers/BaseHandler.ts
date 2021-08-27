5888b87d6de0fea4f4e3e7b37b642a4fff3a989c
Eric Chang <ericsonc510@gmail.com>
2021-08-27 12:06:34 +0800

    feat(core/handler): [BaseHandler] scrape crime statistics

--- src/crimegrade/core/handlers/BaseHandler.ts
+++ src/crimegrade/core/handlers/BaseHandler.ts
@@ -15,6 +15,28 @@ const CSS_SELECTORS = {
   OVERALL_GRADE: '.overallGradeLetter',
 }
 
+const XPATH_SELECTOR = {
+  STATS_TABLE: (category) => `//h3[contains(text(),'${category} Crime Rates')]/following-sibling::table`,
+}
+
+enum CRIME_CATEGORY {
+  VIOLENT = 'Violent',
+  PROPERTY = 'Property',
+  OTHER = 'Other',
+};
+
+type CrimeStatistic = {
+  catetory: CRIME_CATEGORY;
+  rate: number;
+  grade: string;
+  details: CrimeStatisticDetail[];
+}
+
+type CrimeStatisticDetail = {
+  type: string;
+  rate: number;
+}
+
 export class BaseHandler extends AbstractHandler {
   protected name: string;
   protected log: Log;
@@ -31,7 +53,8 @@ export class BaseHandler extends AbstractHandler {
       this.log.info('Start handling task', { url: page.url() });
 
       const baseInfo = await this.getBaseInfo(page);
-      const result = Object.assign({},  baseInfo);
+      const statistics = await this.getStatistics(page);
+      const result = Object.assign({},  baseInfo, statistics);
 
       if (saveResult) {
         await pushData(result);
@@ -62,5 +85,54 @@ export class BaseHandler extends AbstractHandler {
       'mapImage': mapImageLink,
       'grade': overallGrade,
     };
+  }
+
+  private async getStatistics(page: Page) {
+    const result: CrimeStatistic[] = [];
+
+    for (let category of Object.values(CRIME_CATEGORY)) {
+      const stat = await this.getStatisticByCategory(page, category);
+      result.push(stat);
+    }
+
+    return { statistics: result };
+  }
+
+  private async getStatisticByCategory(page: Page, category: CRIME_CATEGORY) {
+    const result = {} as CrimeStatistic;
+    const table = (await page.$x(XPATH_SELECTOR.STATS_TABLE(category)))[0];
+    const rows = await table.$x('./tbody/tr');
+    const dataRows = [];
+
+    rows.shift();
+
+    for (let row of rows) {
+      const emptyCells = await row.$x("./td[contains(@class, 'blankRow')]");
+
+      if (emptyCells.length === 0) {
+        dataRows.push(row);
+      };
+    }
+
+    const totalRow = dataRows.pop();
+    const totalValue = await (await totalRow.$x('./td[2]/div'))[0].evaluate(element => element.innerHTML);
+    const [totalRate, totalGrade] = totalValue.split(' ');
+
+    result.catetory = category;
+    result.rate = Number(totalRate);
+    result.grade = totalGrade.replace(/[\(\)]/g, '');
+    result.details = [];
+
+    for (let row of dataRows) {
+      const crimeType = await (await row.$x('./td[1]/div'))[0].evaluate(element => element.innerHTML);
+      const crimeRate = await (await row.$x('./td[2]/div'))[0].evaluate(element => element.innerHTML);
+
+      result.details.push({
+        type: crimeType,
+        rate: Number(crimeRate),
+      });
+    }
+
+    return result;
   }
 }
